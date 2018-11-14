54b028d07f4c6cd77be6fd94191d5a985276a991
Moty Michaely <moty@xplenty.com>
2018-11-14 10:00:03 +0200

    v1.0

--- test/help_test.sh
+++ test/help_test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-
 set -ex
 
-aws-iam-authenticator help
+. ${BUILDPACK_TEST_RUNNER_HOME}/lib/test_utils.sh
+
+testAwsIamAuthenticatorExist()
+{
+  compile
+  assertCapturedSuccess
+  #assertFileMD5 "bca743e1573a4fee2576365a180dfe97" ${BUILD_DIR}/.aws-iam-authenticator/aws-iam-authenticator
+}
